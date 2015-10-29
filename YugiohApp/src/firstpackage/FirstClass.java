28672bed5a9f7e1e0fd0b755fc0099fb34bd4662
Sam McConnell <sam5binny@Sams-MacBook-Pro.local>
2015-10-28 21:35:32 -0700

    ajkdslkasdjkldsalk
    Hello jkajkhdsahjkasjkhdsa

--- YugiohApp/src/firstpackage/FirstClass.java
+++ YugiohApp/src/firstpackage/FirstClass.java
@@ -13,7 +13,7 @@ public class FirstClass {
      */
     private static void createAndShowGUI() {
         //Create and set up the window.
-        JFrame frame = new JFrame("FrameDemo");
+        JFrame frame = new JFrame("c-ASS-SAND-dra");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         JLabel emptyLabel = new JLabel("");
