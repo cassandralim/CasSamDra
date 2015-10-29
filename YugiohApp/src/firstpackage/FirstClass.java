df2cf6b8f124a6cf781fdc61f2d9b51c217a26f9
cassandralim <cassandralim.tmd@gmail.com>
2015-10-28 21:49:29 -0700

    sam

--- YugiohApp/src/firstpackage/FirstClass.java
+++ YugiohApp/src/firstpackage/FirstClass.java
@@ -13,11 +13,11 @@ public class FirstClass {
      */
     private static void createAndShowGUI() {
         //Create and set up the window.
-        JFrame frame = new JFrame("c-ASS-SAND-dra");
+        JFrame frame = new JFrame("Sam has monkey butt");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         JLabel emptyLabel = new JLabel("");
-        emptyLabel.setPreferredSize(new Dimension(175, 100));
+        emptyLabel.setPreferredSize(new Dimension(1750, 1000));
         frame.getContentPane().add(emptyLabel, BorderLayout.CENTER);
 
         //Display the window.
